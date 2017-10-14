29d2b67e9ed2fff67f9d3a28f78480a8eb20ed33
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-14 20:18:40 +0300

    first solution with traditional looping

---
+++ index.js
@@ -0,0 +1,31 @@
+var countWater = function(array) {
+
+    var step = 0;
+        left = 0;
+        total = 0;
+        right = array[array.length - 1];
+
+    array.forEach(function (elem, index) {
+        if (left > elem) {
+            total += left - elem;
+        } else {
+            left = elem;
+            step = index;
+        }
+    });
+
+    for (i = array.length - 1; i > step; i--) {
+        array[i] > right ? right = array[i] : right;
+        total -= array[step] - right;
+    }
+
+    return total;
+};
+
+console.log(countWater([2, 5, 1, 3, 1, 2, 1, 7, 7, 6]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
+console.log(countWater([7, 0, 1, 3, 4, 1, 2, 1]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
+console.log(countWater([2, 2, 1, 2, 2, 3, 0, 1, 2]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8]));
+console.log(countWater([2, 2, 2, 2, 2]));
