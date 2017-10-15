67e85374bf70e93258cbe02cd60412ced0abf468
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-15 08:46:12 +0300

    uncomment console.log()

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ var countWater = function(array) {
 };
 
 console.log(countWater([2, 5, 1, 3, 1, 2, 1, 7, 7, 6]));
-// console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
-// console.log(countWater([7, 0, 1, 3, 4, 1, 2, 1]));
-// console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
-// console.log(countWater([2, 2, 1, 2, 2, 3, 0, 1, 2]));
-// console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8]));
-// console.log(countWater([2, 2, 2, 2, 2]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
+console.log(countWater([7, 0, 1, 3, 4, 1, 2, 1]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0]));
+console.log(countWater([2, 2, 1, 2, 2, 3, 0, 1, 2]));
+console.log(countWater([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8]));
+console.log(countWater([2, 2, 2, 2, 2]));
